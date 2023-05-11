bee63cba92b91e5a1100035cb9bb11a2a2cb5cfb
Yaneth <yaneth.rodriguez@uxweb.com.co>
2023-05-10 22:06:02 -0500

    solución a los ejercicios de js

---
+++ js/hello.js
@@ -0,0 +1,9 @@
+// escribe la función hola acá
+function hello(name) {
+    return "Hola " + name;
+  }
+
+// código de prueba
+console.log(hello("Pedro")) // "Hola Pedro!"
+console.log(hello("Juan")) // "Hola Juan!"
+console.log(hello("Yaneth Rodríguez")) // "Hola !"
---
+++ js/sum.js
@@ -0,0 +1,9 @@
+// escribe la función suma acá
+function suma(positivo) {
+    return ((positivo*(positivo+1))/2);
+  }
+
+// código de prueba
+console.log(suma(4)) // 1 + 2 + 3 + 4 = 10
+console.log(suma(10)) // 55
+console.log(suma(15)) // 120
